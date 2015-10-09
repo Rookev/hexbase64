2405356344232b7df08b335a5f5996d354727d59
Rookev <kevin.roos@web.de>
2015-10-09 14:51:21 +0200

    First raw working draft of conversion functions

--- javascripts/functions.js
+++ javascripts/functions.js
@@ -1,17 +1,38 @@
+/**
+ * Handler for toBase64Button
+ */
 function btnToBase64_Clicked()
 {
 	var originContent = getContentFromOriginTextarea();
-	alert('to base64: ' + originContent);
+	var encodedData = window.btoa(originContent);
+
+	setContentToTargetTextarea(encodedData);
 }
 
+/**
+ * Handler for toHexButton
+ */
 function btnToHex_Clicked()
 {
 	var originContent = getContentFromOriginTextarea();
-	alert('to hex: ' + originContent);
+	var decodedData = window.atob(originContent);
+
+	setContentToTargetTextarea(decodedData);
 }
 
+/**
+ * Returns the content of the origin textarea
+ */
 function getContentFromOriginTextarea()
 {
 	var originContent = document.getElementById("textarea-origin-content").value;
 	return originContent;
+}
+
+/**
+ * Sets the content of the target textarea
+ */
+function setContentToTargetTextarea(content)
+{
+	document.getElementById("textarea-target-content").value = content;
 }
